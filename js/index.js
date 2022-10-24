7a9a2214055bc27f3589be4ac1c480eb58b3efe9
richardjohnagrida <agrida.richardjohn@gmail.com>
2022-10-24 09:46:24 +0800

    update property listing

---
+++ js/index.js
@@ -0,0 +1,29 @@
+function storeApt1 () {
+    location.href='appartment.html'
+    localStorage.setItem("text", "apt1")
+}
+
+function storeApt2 () {
+    location.href='appartment.html'
+    localStorage.setItem("text", "apt2")
+}
+
+function storeApt3 () {
+    location.href='appartment.html'
+    localStorage.setItem("text", "apt3")
+}
+
+function storeApt4 () {
+    location.href='appartment.html'
+    localStorage.setItem("text", "apt4")
+}
+
+function storeApt5 () {
+    location.href='appartment.html'
+    localStorage.setItem("text", "apt5")
+}
+
+function storeApt6 () {
+    location.href='appartment.html'
+    localStorage.setItem("text", "apt6")
+}
